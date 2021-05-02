e62bc9074f201fb22fd2853d322afa410ff75a9a
SimonVenier <70879289+SimonVenier@users.noreply.github.com>
2021-05-02 15:28:51 +0200

    Ausgangsposition =D

---
+++ Buchhandlung/src/main/java/at/itkollegimst/venier/pos1makro/test2/buchhandlung/infrastructure/repos/RepoBestellung.java
@@ -0,0 +1,8 @@
+package at.itkollegimst.venier.pos1makro.test2.buchhandlung.infrastructure.repos;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepoBestellung {
+
+}
---
+++ order/src/main/java/at/itkollegimst/venier/pos1makro/test2/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.venier.pos1makro.test2.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
