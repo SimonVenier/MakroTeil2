3ec987c483fa8634e4122645a34735391a33a6ca
SimonVenier <70879289+SimonVenier@users.noreply.github.com>
2021-05-02 15:27:10 +0200

    Ausgangsposition =D

--- src/main/java/at/itkollegimst/venier/pos1makro/test2/buchhandlung/infrastructure/repos/RepoBestellung.java
+++
@@ -1,8 +0,0 @@
-package at.itkollegimst.venier.pos1makro.test2.buchhandlung.infrastructure.repos;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepoBestellung {
-
-}
